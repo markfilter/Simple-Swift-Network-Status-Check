359cbe3700dbc9bfc2c6f4f36ab4fc72a53f4e68
Mark Filter <markfilter@outlook.com>
2018-02-23 13:09:38 -0500

    Adds Storyboard UI

--- Simple Network Status Checker/Simple Network Status Checker/ViewController.swift
+++ Simple Network Status Checker/Simple Network Status Checker/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var lbl_resultsLabel: UILabel!
+    @IBOutlet var tf_targetUrlTextField: UITextField!
+    @IBOutlet var btn_CheckStatusButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        disableButton()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Custom Methods
+    
+    func enableButton() {
+        btn_CheckStatusButton.alpha = 1.0
+        btn_CheckStatusButton.isEnabled = true
+    }
+    
+    func disableButton() {
+        btn_CheckStatusButton.alpha = 0.5
+        btn_CheckStatusButton.isEnabled = false
+    }
 
 }
 
