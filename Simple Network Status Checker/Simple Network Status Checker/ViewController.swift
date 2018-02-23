eff1d8ec3ee4f6ad109eddb4408683bdb028ddae
Mark Filter <markfilter@outlook.com>
2018-02-23 13:19:19 -0500

    Adds program logic for checking network status. Needs TextWatcher to enable button when target URL is added

--- Simple Network Status Checker/Simple Network Status Checker/ViewController.swift
+++ Simple Network Status Checker/Simple Network Status Checker/ViewController.swift
@@ -4,7 +4,20 @@
 //
 //  Created by Mark Filter on 2/23/18.
 //  Copyright © 2018 Mark Filter. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//  and associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell coppies of the software and to permit persons to whome the Software is
+//  furnished to do so, subject to the following conditions:
 //
+//  The above copyright notice and this permission notice shall be included in all copies or
+//  substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+//  NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import UIKit
 
@@ -25,6 +38,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        NetworkUtils.checkConnection(viewController: self) { (alertController, isConnected) -> (Void) in
+            if isConnected {
+                self.lbl_resultsLabel.text = "Connected successfully!"
+            }
+            else {
+                 self.lbl_resultsLabel.text = "Error connecting to server!"
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func buttonCheckStatusTapped(_ sender: UIButton) {
+        if btn_CheckStatusButton.isEnabled == true {
+            NetworkUtils.checkConnection(viewController: self) { (alertController, isConnected) -> (Void) in
+                if isConnected {
+                    self.lbl_resultsLabel.text = "Connected successfully!"
+                }
+                else {
+                    self.lbl_resultsLabel.text = "Error connecting to server!"
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
 
     // MARK: - Custom Methods
     
